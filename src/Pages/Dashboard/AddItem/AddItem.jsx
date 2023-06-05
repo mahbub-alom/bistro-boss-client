350fa8c2079d9de9e25c42d3a2cd8362b67b476a
Md Mahbub Alom <mdmahbuba034@gmail.com>
2023-06-05 22:31:31 +0600

    img hosting done

--- src/Pages/Dashboard/AddItem/AddItem.jsx
+++ src/Pages/Dashboard/AddItem/AddItem.jsx
@@ -3,14 +3,27 @@ import SectionTitle from "../../../Components/SectionTitle";
 import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
 
+const img_hosting_token = import.meta.env.VITE_IMAGE_UPLOAD_TOKEN;
+
 const AddItem = () => {
+    const img_hosting_url = `https://api.imgbb.com/1/upload?expiration=600&key=${img_hosting_token}`;
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
   const onSubmit = (data) => {
-    console.log(data);
+      const formData = new FormData();
+      formData.append('image', data.image[0])
+      
+      fetch(img_hosting_url, {
+          method: 'POST',
+          body:formData
+      })
+          .then(res => res.json())
+          .then(imgResponse => {
+          console.log(imgResponse);
+      })
   };
   console.log(errors);
 
@@ -41,11 +54,12 @@ const AddItem = () => {
             <label className="label">
               <span className="label-text">Category*</span>
             </label>
-            <select
+                      <select
+                      defaultValue="Pick One"
               {...register("category", { required: true })}
               className="select select-bordered"
             >
-              <option disabled selected>
+              <option disabled>
                 Pick one
               </option>
               <option>Pizza</option>
@@ -83,7 +97,7 @@ const AddItem = () => {
           </label>
           <input
             type="file"
-            {...register("image", { required: true})}
+            {...register("image", { required: true })}
             className="file-input file-input-bordered w-full"
           />
         </div>
